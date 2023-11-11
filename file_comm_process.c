9de745d2ef364af134a30cc04310daa6e5b9af51
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-11-11 08:54:22 -0800

    more updates

--- file_comm_process.c
+++ file_comm_process.c
@@ -4,11 +4,11 @@ int cant_open(char *file_path);
 int proc_file_commands(char *file_path, int *exe_ret);
 
 /**
- * cant_open - If the file doesn't exist or lacks proper permissions, print
- * a cant open error.
- * @file_path: Path to the supposed file.
+ * cant_open - this function prints a cant open error, If the file
+ *	doesn't exist or lacks proper permissions
+ * @file_path: the path to the supposed file
  *
- * Return: 127.
+ * Return: 127
  */
 
 int cant_open(char *file_path)
@@ -42,14 +42,14 @@ int cant_open(char *file_path)
 }
 
 /**
- * proc_file_commands - Takes a file and attempts to run the commands stored
- * within.
- * @file_path: Path to the file.
- * @exe_ret: Return value of the last executed command.
+ * proc_file_commands - function that takes a file and attempts to run
+ * the commands stored within
+ * @file_path: Path to the file
+ * @exe_ret: Return value of the last executed command
  *
- * Return: If file couldn't be opened - 127.
- *	   If malloc fails - -1.
- *	   Otherwise the return value of the last command ran.
+ * Return: If file couldn't be opened - 127
+ *	   If malloc fails - -1
+ *	   Otherwise the return value of the last command ran
  */
 int proc_file_commands(char *file_path, int *exe_ret)
 {
