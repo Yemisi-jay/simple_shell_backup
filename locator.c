2556423ba048d01ab48c232cba7ee29561d5b5bd
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-11-12 12:25:07 -0800

    locator file updated

--- locator.c
+++ locator.c
@@ -4,11 +4,11 @@ char *fill_path_dir(char *path);
 list_t *get_path_dir(char *path);
 
 /**
- * get_location - Locates a command in the PATH.
- * @command: The command to locate.
+ * get_location - function that locates a command in the PATH
+ * @command: The command to locate
  *
- * Return: If an error occurs or the command cannot be located - NULL.
- *         Otherwise - the full pathname of the command.
+ * Return: If an error occurs or the command cannot be located - NULL
+ *         Otherwise return - the full pathname of the command
  */
 char *get_location(char *command)
 {
@@ -49,12 +49,12 @@ char *get_location(char *command)
 }
 
 /**
- * fill_path_dir - Copies path but also replaces leading/sandwiched/trailing
- *		   colons (:) with current working directory.
- * @path: The colon-separated list of directories.
+ * fill_path_dir - this function copies path but also replaces leading
+ * /sandwiched/trailing colons (:) with current working directory
+ * @path: The colon-separated list of directories
  *
  * Return: A copy of path with any leading/sandwiched/trailing colons replaced
- *	   with the current working directory.
+ *	   with the current working directory
  */
 char *fill_path_dir(char *path)
 {
@@ -104,11 +104,11 @@ char *fill_path_dir(char *path)
 }
 
 /**
- * get_path_dir - Tokenizes a colon-separated list of
- *                directories into a list_s linked list.
- * @path: The colon-separated list of directories.
+ * get_path_dir - function that tokenizes a colon-separated list of
+ *                directories into a list_s linked list
+ * @path: The colon-separated list of directories
  *
- * Return: A pointer to the initialized linked list.
+ * Return: this returns a pointer to the initialized linked list
  */
 list_t *get_path_dir(char *path)
 {
