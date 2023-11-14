577ab3beaf0895bef31aad94d2b94e378cc5824d
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-11-14 14:13:12 +0100

    final changes

--- environment.c
+++ environment.c
@@ -1,135 +1,75 @@
 #include "shell.h"
 
-int shellby_env(char **args, char __attribute__((__unused__)) **front);
-int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
-int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
+char **_copyenv(void);
+void free_environ(void);
+char **_getenv(const char *var);
 
 /**
- * shellby_env - function that prints the current environment
- * @args: An array of arguments passed to the shell
- * @front: A double pointer to the beginning of arguments
+ * _copyenv - Creates a copy of the environment.
  *
- * Return: If an error occurs - -1
- *	   Otherwise return - 0
- *
- * Description: The function prints one variable per line in the
- *              format 'variable'='value'
+ * Return: If an error occurs - NULL.
+ *         O/w - a double pointer to the new copy.
  */
-int shellby_env(char **args, char __attribute__((__unused__)) **front)
+char **_copyenv(void)
 {
-	int index;
-	char nc = '\n';
-
-	if (!environ)
-		return (-1);
-
-	for (index = 0; environ[index]; index++)
-	{
-		write(STDOUT_FILENO, environ[index], _strlen(environ[index]));
-		write(STDOUT_FILENO, &nc, 1);
-	}
-
-	(void)args;
-	return (0);
-}
-
-/**
- * shellby_setenv - function that changes or adds an environmental
- *	variable to the PATH
- * @args: An array of arguments passed to the shell
- * @front: A double pointer to the beginning of arguments
- * Description: args[1] is the name of the new or existing PATH variable
- *              args[2] is the value to set the new or changed variable to
- *
- * Return: If an error occurs - -1
- *         Otherwise return - 0
- */
-int shellby_setenv(char **args, char __attribute__((__unused__)) **front)
-{
-	char **env_var = NULL, **new_environ, *new_value;
+	char **new_environ;
 	size_t size;
 	int index;
-
-	if (!args[0] || !args[1])
-		return (create_error(args, -1));
-
-	new_value = malloc(_strlen(args[0]) + 1 + _strlen(args[1]) + 1);
-	if (!new_value)
-		return (create_error(args, -1));
-	_strcpy(new_value, args[0]);
-	_strcat(new_value, "=");
-	_strcat(new_value, args[1]);
-
-	env_var = _getenv(args[0]);
-	if (env_var)
-	{
-		free(*env_var);
-		*env_var = new_value;
-		return (0);
-	}
-	for (size = 0; environ[size]; size++)
-		;
-
-	new_environ = malloc(sizeof(char *) * (size + 2));
-	if (!new_environ)
-	{
-		free(new_value);
-		return (create_error(args, -1));
-	}
-
-	for (index = 0; environ[index]; index++)
-		new_environ[index] = environ[index];
-
-	free(environ);
-	environ = new_environ;
-	environ[index] = new_value;
-	environ[index + 1] = NULL;
-
-	return (0);
-}
-
-/**
- * shellby_unsetenv - function that deletes an environmental variable
- *	from the PATH
- * @args: array of arguments passed to the shell
- * @front: A double pointer to the beginning of arguments
- * Description: args[1] is the PATH variable to remove
- *
- * Return: If an error occurs - -1
- *         Otherwise return - 0
- */
-int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front)
-{
-	char **env_var, **new_environ;
-	size_t size;
-	int index, index2;
-
-	if (!args[0])
-		return (create_error(args, -1));
-	env_var = _getenv(args[0]);
-	if (!env_var)
-		return (0);
 
 	for (size = 0; environ[size]; size++)
 		;
 
-	new_environ = malloc(sizeof(char *) * size);
+	new_environ = malloc(sizeof(char *) * (size + 1));
 	if (!new_environ)
-		return (create_error(args, -1));
+		return (NULL);
 
-	for (index = 0, index2 = 0; environ[index]; index++)
+	for (index = 0; environ[index]; index++)
 	{
-		if (*env_var == environ[index])
+		new_environ[index] = malloc(_strlen(environ[index]) + 1);
+
+		if (!new_environ[index])
 		{
-			free(*env_var);
-			continue;
+			for (index--; index >= 0; index--)
+				free(new_environ[index]);
+			free(new_environ);
+			return (NULL);
 		}
-		new_environ[index2] = environ[index];
-		index2++;
+		_strcpy(new_environ[index], environ[index]);
 	}
-	free(environ);
-	environ = new_environ;
-	environ[size - 1] = NULL;
+	new_environ[index] = NULL;
 
-	return (0);
+	return (new_environ);
+}
+
+/**
+ * free_environ - Frees the the environment copy.
+ */
+void free_environ(void)
+{
+	int index;
+
+	for (index = 0; environ[index]; index++)
+		free(environ[index]);
+	free(environ);
+}
+
+/**
+ * _getenv - Gets an environmental variable from the PATH.
+ * @var: The name of the environmental variable to get.
+ *
+ * Return: If the environmental variable does not exist - NULL.
+ *         Otherwise - a pointer to the environmental variable.
+ */
+char **_getenv(const char *var)
+{
+	int index, len;
+
+	len = _strlen(var);
+	for (index = 0; environ[index]; index++)
+	{
+		if (_strncmp(var, environ[index], len) == 0)
+			return (&environ[index]);
+	}
+
+	return (NULL);
 }
--- helpers_input.c
+++ helpers_input.c
@@ -1,188 +1,209 @@
 #include "shell.h"
 
-void handle_line(char **line, ssize_t read);
-ssize_t get_new_len(char *line);
-void logical_ops(char *line, ssize_t *new_len);
+char *args_get(char *line, int *exe_ret);
+int args_calls(char **args, char **front, int *exe_ret);
+int args_run(char **args, char **front, int *exe_ret);
+int args_handler(int *exe_ret);
+int args_checker(char **args);
 
 /**
- * handle_line - function that partitions a line read from standard input
- * as needed
- * @line: A pointer to a line read from standard input
- * @read: The length of line
+ * args_get - Gets a command from standard input.
+ * @line: A buffer to store the command.
+ * @exe_ret: The return value of the last executed command.
  *
- * Description: Spaces are inserted to separate ";", "||", and "&&"
- *              Replaces "#" with '\0'
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the stored command.
  */
-void handle_line(char **line, ssize_t read)
+char *args_get(char *line, int *exe_ret)
 {
-	char *old_line, *new_line;
-	char previous, current, next;
-	size_t i, j;
-	ssize_t new_len;
+	size_t n = 0;
+	ssize_t read;
+	char *prompt = "$ ";
 
-	new_len = get_new_len(*line);
-	if (new_len == read - 1)
-		return;
-	new_line = malloc(new_len + 1);
-	if (!new_line)
-		return;
-	j = 0;
-	old_line = *line;
-	for (i = 0; old_line[i]; i++)
+	if (line)
+		free(line);
+
+	read = _getline(&line, &n, STDIN_FILENO);
+	if (read == -1)
+		return (NULL);
+	if (read == 1)
 	{
-		current = old_line[i];
-		next = old_line[i + 1];
-		if (i != 0)
-		{
-			previous = old_line[i - 1];
-			if (current == ';')
-			{
-				if (next == ';' && previous != ' ' && previous != ';')
-				{
-					new_line[j++] = ' ';
-					new_line[j++] = ';';
-					continue;
-				}
-				else if (previous == ';' && next != ' ')
-				{
-					new_line[j++] = ';';
-					new_line[j++] = ' ';
-					continue;
-				}
-				if (previous != ' ')
-					new_line[j++] = ' ';
-				new_line[j++] = ';';
-				if (next != ' ')
-					new_line[j++] = ' ';
-				continue;
-			}
-			else if (current == '&')
-			{
-				if (next == '&' && previous != ' ')
-					new_line[j++] = ' ';
-				else if (previous == '&' && next != ' ')
-				{
-					new_line[j++] = '&';
-					new_line[j++] = ' ';
-					continue;
-				}
-			}
-			else if (current == '|')
-			{
-				if (next == '|' && previous != ' ')
-					new_line[j++]  = ' ';
-				else if (previous == '|' && next != ' ')
-				{
-					new_line[j++] = '|';
-					new_line[j++] = ' ';
-					continue;
-				}
-			}
-		}
-		else if (current == ';')
-		{
-			if (i != 0 && old_line[i - 1] != ' ')
-				new_line[j++] = ' ';
-			new_line[j++] = ';';
-			if (next != ' ' && next != ';')
-				new_line[j++] = ' ';
-			continue;
-		}
-		new_line[j++] = old_line[i];
+		hist++;
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, prompt, 2);
+		return (args_get(line, exe_ret));
 	}
-	new_line[j] = '\0';
 
-	free(*line);
-	*line = new_line;
+	line[read - 1] = '\0';
+	variable_replacement(&line, exe_ret);
+	handle_line(&line, read);
+
+	return (line);
 }
 
 /**
- * get_new_len - this function gets the new length of a line partitioned
- *               by ";", "||", "&&&", or "#"
- * @line: The line to be checked
+ * args_calls - Partitions operators from commands and calls them.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
+ * @exe_ret: The return value of the parent process' last executed command.
  *
- * Return: The new length of the line
- *
- * Description: shorten lines containing '#' comments with '\0'
+ * Return: The return value of the last executed command.
  */
-
-ssize_t get_new_len(char *line)
+int args_calls(char **args, char **front, int *exe_ret)
 {
-	size_t i;
-	ssize_t new_len = 0;
-	char current, next;
+	int ret, index;
 
-	for (i = 0; line[i]; i++)
+	if (!args[0])
+		return (*exe_ret);
+	for (index = 0; args[index]; index++)
 	{
-		current = line[i];
-		next = line[i + 1];
-		if (current == '#')
+		if (_strncmp(args[index], "||", 2) == 0)
 		{
-			if (i == 0 || line[i - 1] == ' ')
+			free(args[index]);
+			args[index] = NULL;
+			args = replace_aliases(args);
+			ret = args_run(args, front, exe_ret);
+			if (*exe_ret != 0)
 			{
-				line[i] = '\0';
-				break;
-			}
-		}
-		else if (i != 0)
-		{
-			if (current == ';')
-			{
-				if (next == ';' && line[i - 1] != ' ' && line[i - 1] != ';')
-				{
-					new_len += 2;
-					continue;
-				}
-				else if (line[i - 1] == ';' && next != ' ')
-				{
-					new_len += 2;
-					continue;
-				}
-				if (line[i - 1] != ' ')
-					new_len++;
-				if (next != ' ')
-					new_len++;
+				args = &args[++index];
+				index = 0;
 			}
 			else
-				logical_ops(&line[i], &new_len);
+			{
+				for (index++; args[index]; index++)
+					free(args[index]);
+				return (ret);
+			}
 		}
-		else if (current == ';')
+		else if (_strncmp(args[index], "&&", 2) == 0)
 		{
-			if (i != 0 && line[i - 1] != ' ')
-				new_len++;
-			if (next != ' ' && next != ';')
-				new_len++;
+			free(args[index]);
+			args[index] = NULL;
+			args = replace_aliases(args);
+			ret = args_run(args, front, exe_ret);
+			if (*exe_ret == 0)
+			{
+				args = &args[++index];
+				index = 0;
+			}
+			else
+			{
+				for (index++; args[index]; index++)
+					free(args[index]);
+				return (ret);
+			}
 		}
-		new_len++;
 	}
-	return (new_len);
+	args = replace_aliases(args);
+	ret = args_run(args, front, exe_ret);
+	return (ret);
 }
+
 /**
- * logical_ops - function that Checks a line for logical operators
- * "||" or "&&"
- * @line: A pointer to the character to be checked in the line
- * @new_len: Pointer to new_len in get_new_len function
+ * args_run - Calls the execution of a command.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
+ * @exe_ret: The return value of the parent process' last executed command.
+ *
+ * Return: The return value of the last executed command.
  */
-void logical_ops(char *line, ssize_t *new_len)
+int args_run(char **args, char **front, int *exe_ret)
 {
-	char previous, current, next;
+	int ret, i;
+	int (*builtin)(char **args, char **front);
 
-	previous = *(line - 1);
-	current = *line;
-	next = *(line + 1);
+	builtin = get_builtin(args[0]);
 
-	if (current == '&')
+	if (builtin)
 	{
-		if (next == '&' && previous != ' ')
-			(*new_len)++;
-		else if (previous == '&' && next != ' ')
-			(*new_len)++;
+		ret = builtin(args + 1, front);
+		if (ret != EXIT)
+			*exe_ret = ret;
 	}
-	else if (current == '|')
+	else
 	{
-		if (next == '|' && previous != ' ')
-			(*new_len)++;
-		else if (previous == '|' && next != ' ')
-			(*new_len)++;
+		*exe_ret = execute(args, front);
+		ret = *exe_ret;
 	}
+
+	hist++;
+
+	for (i = 0; args[i]; i++)
+		free(args[i]);
+
+	return (ret);
+}
+
+/**
+ * args_handler - Gets, calls, and runs the execution of a command.
+ * @exe_ret: The return value of the parent process' last executed command.
+ *
+ * Return: If an end-of-file is read - END_OF_FILE (-2).
+ *         If the input cannot be tokenized - -1.
+ *         O/w - The exit value of the last executed command.
+ */
+int args_handler(int *exe_ret)
+{
+	int ret = 0, index;
+	char **args, *line = NULL, **front;
+
+	line = args_get(line, exe_ret);
+	if (!line)
+		return (END_OF_FILE);
+
+	args = _strtok(line, " ");
+	free(line);
+	if (!args)
+		return (ret);
+	if (args_checker(args) != 0)
+	{
+		*exe_ret = 2;
+		args_free(args, args);
+		return (*exe_ret);
+	}
+	front = args;
+
+	for (index = 0; args[index]; index++)
+	{
+		if (_strncmp(args[index], ";", 1) == 0)
+		{
+			free(args[index]);
+			args[index] = NULL;
+			ret = args_calls(args, front, exe_ret);
+			args = &args[++index];
+			index = 0;
+		}
+	}
+	if (args)
+		ret = args_calls(args, front, exe_ret);
+
+	free(front);
+	return (ret);
+}
+
+/**
+ * args_checker - Checks if there are any leading ';', ';;', '&&', or '||'.
+ * @args: 2D pointer to tokenized commands and arguments.
+ *
+ * Return: If a ';', '&&', or '||' is placed at an invalid position - 2.
+ *	   Otherwise - 0.
+ */
+int args_checker(char **args)
+{
+	size_t i;
+	char *cur, *nex;
+
+	for (i = 0; args[i]; i++)
+	{
+		cur = args[i];
+		if (cur[0] == ';' || cur[0] == '&' || cur[0] == '|')
+		{
+			if (i == 0 || cur[1] == ';')
+				return (create_error(&args[i], 2));
+			nex = args[i + 1];
+			if (nex && (nex[0] == ';' || nex[0] == '&' || nex[0] == '|'))
+				return (create_error(&args[i + 1], 2));
+		}
+	}
+	return (0);
 }
